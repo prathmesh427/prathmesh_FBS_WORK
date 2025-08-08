c770b9c1eb63d04169691760e8cf147ae9c41960
prathmesh427 <prathmeshlakde007@gmail.com>
2025-08-08 07:46:27 +0530

    Add files via upload

---
+++ Assignments/assignment1/question 5.c
@@ -0,0 +1,15 @@
+//Write a program to check whether a person is eligible to vote (age = 18).
+#include <stdio.h>
+ void main ()
+ {
+ 	int age=18;
+ 	
+ 	if(age<=19)
+ 	{
+ 		printf("eligible to vote");
+	}
+	else
+	{
+		printf("not eligible to vote");
+	}
+ }
---
+++ Assignments/assignment1/question 6.c
@@ -0,0 +1,15 @@
+//Write a program to check whether a given character is uppercase or lowercase. 
+#include <stdio.h>
+ void main()
+ {
+ 	char ch = 'A';
+ 	
+ 	if(ch>='A'&& ch<='Z')
+ 	{
+ 		printf("character is uppercase");
+ 	}
+ 	else
+ 	{
+ 	    printf("character is lowercase");
+	}
+ }
