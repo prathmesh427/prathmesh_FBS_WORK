a7fc9b38fab26d066ebe6a5226669fff8d620299
prathmesh Lakde <prathmeshlakde007@gmail.com>
2025-08-31 15:38:45 +0530

    Add files via upload

---
+++ c module/assignment01/question 1.c
@@ -0,0 +1,15 @@
+//Write a program to check whether a number is even or odd. 
+#include<stdio.h>
+ void main()
+ {
+ 	int no=14;
+ 	
+	  if(no%2==0)
+	  {
+	  	printf("number is even");
+	  }
+	  else
+	  {
+	  	printf("number is odd");
+	  }
+ }
---
+++ c module/assignment01/question 2.c
@@ -0,0 +1,20 @@
+//Write a program to check given 3 digit number is pallindrome or not.
+#include <stdio.h>
+ 
+ void main()
+ {
+ 	int no=121;
+ 	int fd,ld;
+ 	
+	 fd=no/100;
+ 	 ld=no%10;
+	  	
+ 	if(fd==ld)
+ 	{
+ 	  printf(" number is palindrome");
+	}
+ 	else
+ 	{
+ 		printf("number is not palindrome");
+	}
+ }
