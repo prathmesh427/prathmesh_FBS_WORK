6dd52d724517df7b65924b2b0c8a9f362fa28802
prathmesh Lakde <prathmeshlakde007@gmail.com>
2025-08-31 15:28:16 +0530

    Delete c module directory

--- c module/assignment10/1strlen(str).c
+++
@@ -1,9 +0,0 @@
-// use strlen(str)
-#include<stdio.h>
-#include<string.h>
-void main()
-{
-	char str[]="prathmesh";
-	
-	printf("length of the string is: %d",strlen(str));   // calculate no of leter length of array.
-}
