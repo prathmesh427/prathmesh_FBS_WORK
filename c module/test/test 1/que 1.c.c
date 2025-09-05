7564ebb7af9746b38425a8e5187b86d9cf84bcdc
prathmesh Lakde <prathmeshlakde007@gmail.com>
2025-09-05 10:14:09 +0530

    Add files via upload

---
+++ c module/test/test 1/que 1.c.c
@@ -0,0 +1,16 @@
+//Que 1. Convert the time entered in hh,min and sec into total seconds.
+#include <stdio.h>
+void main ()
+{
+  int hrs= 2;
+  int min;
+  int sec;
+  
+     min= hrs*60;
+     sec= min*60;
+     
+     printf("hrs and min in sec is %d",sec);
+     
+}
+     
+     
---
+++ c module/test/test 1/que 3.c.c
@@ -0,0 +1,12 @@
+//Que 3. Print last 2 digit of number.
+//Eg. i/p: 223410
+//    o/p:10
+#include<stdio.h>
+void main()
+{
+	int num=223410,r1,r2,q1;
+	r1=num%100;
+
+	printf("%d",r1);
+	
+}
